6ccd2092c879c3d55d08f1f7884c93383ec3282c
musthafa2001 <e1185026@gmail.com>
2024-05-01 05:35:37 +0000

    transaction calculate

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 require_once '../transaction.php';
 
 
-$transaction= new Transaction;
+$transaction= new Transaction(100,'hello wolrd');
 
+$transaction->addTax(8);
+$transaction->discount(10);
 var_dump($transaction->amount);
--- transaction.php
+++ transaction.php
@@ -1,7 +1,23 @@
 <?php
+
+declare(strict_types=1);
+
 class Transaction{
-    public $amount;
-    public $description;    
+    private float $amount;
+    private string $description;  
+    
+    public function __construct(float $amount , string $description){
+        $this->amount=$amount;
+        $this->description=$description;
+
+    }
+    public function addTax(float $rate){
+        $this->amount += $this->amount*$rate/100;
+    }
+    public function discount(float $rate){
+        $this->amount-=$this->amount* $rate/100;
+    }
+
 }
 
 
